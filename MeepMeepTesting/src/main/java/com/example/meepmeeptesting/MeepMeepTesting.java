cd15c4329a34c226e3b33bf0b9ed76ac8b75a88c
Lia <139262819+92doki@users.noreply.github.com>
2024-10-03 14:56:13 -0700

    Began Sample Autonomous Program - Otylia (10/03/24)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,25 +1,46 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+    private static final double TILE_WIDTH = 23.5;
+    private static final double TILE_HEIGHT = 23.5;
+    private static final double ROBOT_HEIGHT = 18;
+    private static final double SAMPLE_HEIGHT = 3.5;
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-52, 52, Math.PI*3/2))
-                .lineToY(20)
-                .turn(Math.toRadians(90))
-                .lineToX(40)
-                .turn(Math.toRadians(90))
-                .lineToY(52)
+        double robotStartingPositionY = 3*TILE_HEIGHT-ROBOT_HEIGHT/2;
+        double robotStartingPositionX = TILE_WIDTH*1.5;
+        double robotSamplePickupLocationY = TILE_HEIGHT+(SAMPLE_HEIGHT)/2;
+        double robotBasketDeliveryLocationX = 56;
+        double robotBasketDeliveryLocationY = 56;
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(robotStartingPositionX, robotStartingPositionY, Math.toRadians(270)))
+                // Goes to basket and drops off first sample
+                .splineTo(new Vector2d(56,56), Math.toRadians(45))
+                .waitSeconds(1)
+                // Picks up first sample off of the field
+                .setTangent(Math.toRadians(215))
+                .splineToLinearHeading(new Pose2d(38,robotSamplePickupLocationY,Math.toRadians(0)), 0)
+                .waitSeconds(1)
+                // Drops off first sample into the basket
+                .setTangent(Math.toRadians(45))
+                .splineToLinearHeading(new Pose2d(robotBasketDeliveryLocationX,robotBasketDeliveryLocationY,Math.toRadians(45)),Math.toRadians(45))
+                .waitSeconds(1)
+                // Picks up second sample off of the field
+                .setTangent(Math.toRadians(225))
+                .splineToLinearHeading(new Pose2d(robotBasketDeliveryLocationX,robotBasketDeliveryLocationY,Math.toRadians(45)),Math.toRadians(45))
+                // Drops off second sample into the basket
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrivingSample.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MecanumDrivingSample extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // constructor takes in frontLeft, frontRight, backLeft, backRight motors
+        // IN THAT ORDER
+        MecanumDrive drive = new MecanumDrive(
+                new Motor(hardwareMap, "frontLeftMotor", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "frontRightMotor", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "backLeftMotor", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "backRightMotor", Motor.GoBILDA.RPM_312)
+        );
+
+
+        // the extended gamepad object
+        GamepadEx driverOp = new GamepadEx(gamepad1);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+                drive.driveRobotCentric(
+                        driverOp.getLeftX(),
+                        driverOp.getLeftY(),
+                        driverOp.getRightX(),
+                        false
+                );
+            }
+            }
+
+        }
